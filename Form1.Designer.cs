ba4b0d8a7a94ef2c2ee764e0a783d35dfa1cb7ea
Tiger <tagir.tsarkov@gmail.com>
2023-03-04 19:40:40 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace MapMakerZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(165, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "C:\\";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Pristina", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(519, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(310, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Make map data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 453);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 490);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(699, 326);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(130, 27);
+            this.panel1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(586, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "building trie";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(203, 495);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "time/ms";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Location = new System.Drawing.Point(287, 497);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(72, 27);
+            this.panel3.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label7.Location = new System.Drawing.Point(47, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 25);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(627, 362);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "joining";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel5.Controls.Add(this.label11);
+            this.panel5.Location = new System.Drawing.Point(699, 362);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(130, 27);
+            this.panel5.TabIndex = 12;
+            // 
+            // label11
+            // 
+            this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(105, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 25);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "0";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(625, 398);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "sorting";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(699, 396);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(130, 27);
+            this.panel2.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("SimSun-ExtB", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(99, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::MapMakerZ.Properties.Resources.map;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(938, 608);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label11;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Threading;
+
+
+
+namespace MapMakerZ
+{
+
+   
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string id;
+        Node root = new Node();
+        Node node = new Node();
+        public int countdivide = 0;
+        public int countsort = 0;
+        public int temporalis = 0;
+        public int temporalis2 = 0;
+        public string path = "notnot";
+        WayNode roots = new WayNode();
+        WayNode wayNode = new WayNode();
+        List<string[]> templist;
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog c = new OpenFileDialog();
+            if(c.ShowDialog()==DialogResult.OK)
+            {
+                label1.Text = c.FileName;
+                open = true;
+            }
+
+
+        }
+
+        
+        
+
+        bool open = false;
+        int bar = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                path = folder.SelectedPath;
+            }
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+
+        private string[] search (string id)
+        {
+
+            Node node = new Node();
+            node = root;
+            for (int i = 0; i <id.Length; i++)
+            {
+                char c = char.Parse(id.Substring(i, 1));
+
+                if (c - '0' < node.nextAll.Length && node.nextAll[c - '0'] != null)
+                {
+                    node = node.nextAll[c - '0'];
+                }
+                else { return null; }
+                
+
+            }
+            if (node.wordEnd == true)
+            {
+                string[] ne = new string[] { node.lat, node.lon };
+                return ne;
+            }
+            else 
+            {
+                MessageBox.Show(id + " not found");
+                
+            }
+
+            return null;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] found = search(textBox1.Text);
+            MessageBox.Show(found?[0] + " " + found?[1]);
+        }
+
+
+        //making maps
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (open == false)
+            {
+                MessageBox.Show("add file");
+                return;
+            }
+            
+            
+            
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Async = false;
+            settings.IgnoreWhitespace = true;
+
+
+            int counttrie = 0;
+            int temp = 0;
+            int countxml = 0;
+            int countwrite = 0;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            bool first = true;
+
+            XmlReader read = XmlReader.Create(label1.Text, settings);
+
+            //create trie
+            while (read.Read())
+            {
+
+                if (read.Name == "node")
+                {
+
+                    node = root;
+                    XmlReader r = read.ReadSubtree();
+                    XElement el = XElement.Load(r);
+                    string id = el.Attribute("id").Value;
+
+
+                    for (int i = 0; i < id.Length; i++)
+                    {
+
+                        char c = char.Parse(id.Substring(i, 1));
+                        if (node.nextAll[c - '0'] == null)
+                        {
+                            node.nextAll[c - '0'] = new Node();
+                            node = node.nextAll[c - '0'];
+
+                        }
+                        else
+                        {
+                            node = node.nextAll[c - '0'];
+                        }
+                    }
+                    if (node.wordEnd == false)
+                    {
+                        node.wordEnd = true;
+                        node.lat = el.Attribute("lat").Value;
+                        node.lon = el.Attribute("lon").Value;
+                    }
+                    counttrie++;
+                    if (temp + 100000 < counttrie)
+                    {
+                        Thread.Sleep(1 / 1000);
+                        backgroundWorker1.ReportProgress(counttrie);
+                        temp = counttrie;
+                    }
+                }
+
+                
+
+            }
+            backgroundWorker1.ReportProgress(counttrie);
+
+
+
+
+            
+
+            read.Close();
+            XmlReader read2 = XmlReader.Create(label1.Text, settings);
+            temp = 0;
+            string D = "foo";
+            bar++;
+
+
+   
+            //join and conquer
+            while (read2.Read())
+            {
+                wayNode = roots;
+                templist = new List<string[]>();
+
+                if (read2.Name == "way" && read.NodeType!=XmlNodeType.EndElement)
+                {
+                    first = true;
+                    string[] cord = { };
+                    char l = 'c';
+                   
+                    XmlReader r = read2.ReadSubtree();
+                    while (r.Read())
+                    {
+
+
+                        if (r.Name == "nd")
+                        {
+                            
+                            cord = search(r.GetAttribute("ref"));
+
+                            if (first == true)
+                            {
+                                D = (cord[0]+"00").Substring(0,4).Replace(".","")+(cord[1]+"00").Substring(0,4).Replace(".","");
+
+                                templist.Add(cord);
+
+                                first = false;
+                            }
+                            else
+                            {
+                                templist.Add(cord);
+                            }
+                         
+                           
+                            
+
+                        }
+                        else if (r.Name == "tag" && r.GetAttribute("k")=="highway")
+                        {
+                            string[] type = { "highway", r.GetAttribute("v") };
+                            templist.Add( type );
+                            
+                        }
+                        
+
+
+                    }
+
+                    for (int i = 0; i < D.Length; i++)
+                    {
+                       l=char.Parse( D.Substring(i, 1));
+                       if(wayNode.wayNodes[l-'0']==null)
+                        {
+                            wayNode.wayNodes[l- '0'] = new WayNode();
+                            
+                            wayNode = wayNode.wayNodes[l - '0'];
+                            
+                        }
+                        else
+                        {
+                            wayNode = wayNode.wayNodes[l - '0'];
+
+                        }
+
+                    }
+
+                    if (wayNode.id == D)
+                    {
+                        wayNode.listoflists.Add(templist);
+                    }
+                    else
+                    {
+                        wayNode.pathed = path;
+                        wayNode.id = D;
+                        wayNode.listoflists.Add(templist);
+                    }
+
+
+                    countxml++;
+                    if (temp + 10000 < countxml)
+                    {
+                       Thread.Sleep(1 / 1000);
+                        backgroundWorker1.ReportProgress(countxml);
+                        temp = countxml;
+                    }
+                }
+            }
+            bar++;
+
+
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                if (roots.wayNodes[i] != null)
+                {
+                    
+                    roots.wayNodes[i].check();
+                }
+                temporalis += 10;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            watch.Stop();
+            read.Close();
+
+            backgroundWorker1.ReportProgress(countwrite);
+                
+            MessageBox.Show("Map complete"+"\nElapsed time "+watch.ElapsedMilliseconds.ToString()); 
+            
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+            if (bar == 0) { label2.Text = e.ProgressPercentage.ToString(); }
+            if (bar == 1) { label11.Text = e.ProgressPercentage.ToString(); }
+            if (bar == 2) { label5.Text = e.ProgressPercentage.ToString()+"%"; }
+                      
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+    }
+
+    public class Node
+    {
+        public bool wordEnd;
+        public Node[] nextAll;
+        public string lat;
+        public string lon;
+
+        public Node()
+        {
+            nextAll = new Node[10];
+            wordEnd = false;
+        }
+
+    }
+
+    
+
+    public class WayNode
+    {
+        public string id;
+        public string pathed;
+        public string type;
+        public WayNode[] wayNodes;
+        public List<List<string[]>> listoflists;
+        public WayNode()
+        {
+            wayNodes = new WayNode[10];
+            id = "empty";
+            listoflists = new List<List<string[]>>();
+        }
+        public void check()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                    
+                if (wayNodes[i]?.listoflists.Count>0)
+                {
+                    XmlWriter write = XmlWriter.Create(wayNodes[i].pathed + @"\" + "X" + wayNodes[i].id + ".osm");
+                    
+                        write.WriteStartDocument();
+                        write.WriteStartElement("coordinates");
+                        for (int a = 0; a < wayNodes[i].listoflists.Count; a++)
+                        {
+                        write.WriteString("\n");
+                        write.WriteStartElement("way");
+                            for (int b = 0; b < wayNodes[i].listoflists[a].Count; b++)
+                            {
+                                write.WriteString("\n");
+                            if (wayNodes[i].listoflists[a][b][0] == "highway")
+                            {
+
+                                
+                                write.WriteStartElement("Z");
+                                write.WriteAttributeString("highway", wayNodes[i].listoflists[a][b][1]);
+                                write.WriteEndElement();
+                            }
+                            else
+                            {
+                                write.WriteStartElement("C" + b.ToString());
+                                write.WriteAttributeString("lat", wayNodes[i].listoflists[a][b][0]);
+                                write.WriteAttributeString("lon", wayNodes[i].listoflists[a][b][1]);
+                                write.WriteEndElement();
+                            }
+                            }
+                        write.WriteString("\n");
+                        write.WriteEndElement();
+                        }
+                        write.WriteString("\n");
+                        write.WriteEndElement();
+                        write.WriteEndDocument();
+                        write.Flush();
+                        write.Close();
+                    
+                }
+                
+                wayNodes[i]?.check();
+            }
+           
+        }
+
+    }
+
+}
